e3222d5808c745fda964f89f34a7acbfc5bb21d7
Joshua Graham <Joshua.Graham@softwire.com>
2023-06-29 17:21:26 +0100

    Knight moves and rook moves updated

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,37 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let posArray = new Array(0);
+        let startRow = board.findPiece(this).row;
+        let startCol = board.findPiece(this).col;
+
+        const directions = [
+            [2, 1],
+            [2, -1],
+            [-2, 1],
+            [-2, -1],
+            [1, 2],
+            [1, -2],
+            [-1, 2],
+            [-1, -2]
+        ];
+
+        for (const direction of directions) {
+            let row = startRow;
+            let col = startCol;
+
+            if (
+                row + direction[0] >= 0 &&
+                row + direction[0] <= 7 &&
+                col + direction[1] >= 0 &&
+                col + direction[1] <= 7
+                ) {
+                row += direction[0];
+                col += direction[1];
+                posArray.push(new Square(row, col));
+            }
+        }
+
+        return posArray;
     }
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -7,18 +7,33 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        let posArray = new Array(0)
+        let posArray = [];
+        let startRow = board.findPiece(this).row;
+        let startCol = board.findPiece(this).col;
 
-        for (let i= 0; i <= 7; i++) {
-            for (let j = 0; j <= 7; j++) {
-                if (i === board.findPiece(this).row && j === board.findPiece(this).col) {
-                    continue;
-                }
-                else if (i === board.findPiece(this).row || j === board.findPiece(this).col) {
-                    posArray.push(new Square(i, j))
-                }
+        const directions = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1]
+        ];
+
+        for (const direction of directions) {
+            let row = startRow;
+            let col = startCol;
+
+            while (
+                row + direction[0] >= 0 &&
+                row + direction[0] <= 7 &&
+                col + direction[1] >= 0 &&
+                col + direction[1] <= 7
+                ) {
+                row += direction[0];
+                col += direction[1];
+                posArray.push(new Square(row, col));
             }
         }
+
         return posArray;
     }
 }
