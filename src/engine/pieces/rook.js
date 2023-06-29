27767d7b61e9c7024e3b677ba19231a4473e60d2
Joshua Graham <Joshua.Graham@softwire.com>
2023-06-29 16:49:42 +0100

    Rook moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,19 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let posArray = new Array(0)
+        let pos = board.findPiece(this)
+
+        for (let i= 0; i <= 7; i++) {
+            for (let j = 0; j <= 7; j++) {
+                if (i === board.findPiece(this).row && j === board.findPiece(this).col) {
+                    continue;
+                }
+                else if (i === board.findPiece(this).row || j === board.findPiece(this).col) {
+                    posArray.push(new Square(i, j))
+                }
+            }
+        }
+        return posArray;
     }
 }
