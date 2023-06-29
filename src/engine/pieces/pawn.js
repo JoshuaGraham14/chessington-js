a5782edd8f50e7490c7c6f94faf2695d5483a7f3
Joshua Graham <Joshua.Graham@softwire.com>
2023-06-29 16:26:15 +0100

    Pawn moves first moves

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,8 +11,14 @@ export default class Pawn extends Piece {
         let posArray = new Array(0)
         let pos = board.findPiece(this)
         if (this.player === Player.WHITE) {
+            if(board.findPiece(this).row === 1) {
+                posArray.push(new Square(pos.row+2, pos.col));
+            }
             posArray.push(new Square(pos.row+1, pos.col));
         } else {
+            if(board.findPiece(this).row === 6) {
+                posArray.push(new Square(pos.row-2, pos.col));
+            }
             posArray.push(new Square(pos.row-1, pos.col));
         }
         return posArray;
