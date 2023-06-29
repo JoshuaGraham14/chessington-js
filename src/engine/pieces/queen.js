c34e60bf5a8b63034cfb62fbec67f8562594161e
Joshua Graham <Joshua.Graham@softwire.com>
2023-06-29 17:14:35 +0100

    Queen moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,37 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let posArray = new Array(0);
+        let startRow = board.findPiece(this).row;
+        let startCol = board.findPiece(this).col;
+
+        const directions = [
+            [1, 1],
+            [1, -1],
+            [-1, 1],
+            [-1, -1],
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1]
+        ];
+
+        for (const direction of directions) {
+            let row = startRow;
+            let col = startCol;
+
+            while (
+                row + direction[0] >= 0 &&
+                row + direction[0] <= 7 &&
+                col + direction[1] >= 0 &&
+                col + direction[1] <= 7
+                ) {
+                row += direction[0];
+                col += direction[1];
+                posArray.push(new Square(row, col));
+            }
+        }
+
+        return posArray;
     }
 }
